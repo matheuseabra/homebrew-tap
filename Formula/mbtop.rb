dae32135e5267e1c73b49fef9ad3f13c94d05f93
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-23 13:21:31 -0300

    brew: fix architecture-specific release URLs

--- Formula/mbtop.rb
+++ Formula/mbtop.rb
@@ -1,15 +1,12 @@
 class Mbtop < Formula
   desc "Tiny, fast system monitor for terminal dashboard panes"
   homepage "https://github.com/matheuseabra/mbtop"
-  version "0.1.0"
   license "MIT"
 
-  on_arm do
+  if Hardware::CPU.arm?
     url "https://github.com/matheuseabra/mbtop/releases/download/v0.1.0/mbtop-v0.1.0-aarch64-apple-darwin.tar.gz"
     sha256 "64ff63520dfb278ded6f96fdf1c3ac8d5d8f0fbe9dd282c8dbbaa927a1ba247b"
-  end
-
-  on_intel do
+  else
     url "https://github.com/matheuseabra/mbtop/releases/download/v0.1.0/mbtop-v0.1.0-x86_64-apple-darwin.tar.gz"
     sha256 "6277760d7356957eeb11901f1db5aa5cb0b9ed527954785da7072c38b65df597"
   end
