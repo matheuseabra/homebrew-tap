edd2112f1e4d066b429d652bf76dee4c66e25d75
Matheus Seabra <matheusvieiracoelho@gmail.com>
2026-08-28 10:14:21 -0300

    Update tclok formula to 0.1.2

--- Formula/tclok.rb
+++ Formula/tclok.rb
@@ -1,8 +1,8 @@
 class Tclok < Formula
   desc "Dependency-free, resize-responsive terminal clock"
   homepage "https://github.com/matheuseabra/tclok"
-  url "https://github.com/matheuseabra/tclok/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "fcc41b0de57bb5007c525f3c3c6e3d9dbd0f3f511fc443f5946e5355d568b565"
+  url "https://github.com/matheuseabra/tclok/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "0460877b134ff80ec5fa19c39890451780a5b314bcacd5db9e7975e23b28e76f"
   license "MIT"
 
   depends_on "rust" => :build
